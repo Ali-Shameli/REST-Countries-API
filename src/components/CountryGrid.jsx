29b57c65d0ed83ace77c798c80a5f25379307590
Ali <ali.shameli1380@yahoo.com>
2026-07-02 20:33:11 +0330

    feat(ui) intial countrygrid skeleton

---
+++ src/components/CountryGrid.jsx
@@ -0,0 +1,9 @@
+const CountryGrid = () => {
+  return (
+    <section className="grid gap-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {/* Country Cards */}
+    </section>
+  );
+};
+
+export default CountryGrid;
