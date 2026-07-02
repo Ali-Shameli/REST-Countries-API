fe7a2af0de9ab04aa56c7119bd96922ef3b4701b
Ali <ali.shameli1380@yahoo.com>
2026-07-03 02:04:58 +0330

    feat(link): add link to each country

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const CountryCard = ({ country }) => {
   const {
     name,
@@ -8,36 +10,39 @@ const CountryCard = ({ country }) => {
   } = country;
 
   return (
-    <article className="overflow-hidden rounded-md shadow-md">
-      <img
-        src={flags.svg}
-        alt={name}
-        className="h-44 w-full object-cover"
-      />
+    <Link to={`/country/${country.name}`}>
+      <article className="overflow-hidden rounded-md shadow-md">
+        <img
+          src={flags.svg}
+          alt={name}
+          className="h-44 w-full object-cover"
+        />
 
-      <div className="space-y-4 p-6">
-        <h2 className="text-lg font-extrabold">
-          {name}
-        </h2>
+        <div className="space-y-4 p-6">
+          <h2 className="text-lg font-extrabold">
+            {name}
+          </h2>
 
-        <div className="space-y-1 text-sm">
-          <p>
-            <span className="font-semibold">Population:</span>{" "}
-            {population.toLocaleString()}
-          </p>
+          <div className="space-y-1 text-sm">
+            <p>
+              <span className="font-semibold">Population:</span>{" "}
+              {population.toLocaleString()}
+            </p>
 
-          <p>
-            <span className="font-semibold">Region:</span>{" "}
-            {region}
-          </p>
+            <p>
+              <span className="font-semibold">Region:</span>{" "}
+              {region}
+            </p>
 
-          <p>
-            <span className="font-semibold">Capital:</span>{" "}
-            {capital}
-          </p>
+            <p>
+              <span className="font-semibold">Capital:</span>{" "}
+              {capital}
+            </p>
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </Link>
+    
   );
 };
 
