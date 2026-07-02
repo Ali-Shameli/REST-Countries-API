4171b4b2931bf6e6a2096e8fd7d007631dd8f070
Ali <ali.shameli1380@yahoo.com>
2026-07-03 00:51:06 +0330

    feat(data): take countries and maping and send country for countricard with props

--- src/components/CountryGrid.jsx
+++ src/components/CountryGrid.jsx
@@ -1,7 +1,14 @@
-const CountryGrid = () => {
+import CountryCard from "./CountryCard";
+
+const CountryGrid = ({ countries }) => {
   return (
     <section className="grid gap-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {/* Country Cards */}
+      {countries.map((country) => (
+        <CountryCard
+          key={country.alpha3Code}
+          country={country}
+        />
+      ))}
     </section>
   );
 };
