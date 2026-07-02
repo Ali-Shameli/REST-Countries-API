3e9091c9460b8b5a34671ba4249701d15d397fb5
Ali <ali.shameli1380@yahoo.com>
2026-07-03 01:44:09 +0330

    add search logic with recived props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { Search } from "lucide-react";
 
-const SearchBar = () => {
+const SearchBar = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="flex h-14 w-full max-w-md items-center gap-4 rounded-md px-6 shadow-md">
       <Search size={18} />
@@ -8,6 +8,8 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder="Search for a country..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         className="w-full bg-transparent outline-none"
       />
     </div>
