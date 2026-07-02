370012c9ff3170223f8391b2af441407c4f2eaa4
Ali <ali.shameli1380@yahoo.com>
2026-07-02 20:29:40 +0330

    feat(ui): add initial serchbar to search countries

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import { Search } from "lucide-react";
+
+const SearchBar = () => {
+  return (
+    <div className="flex h-14 w-full max-w-md items-center gap-4 rounded-md px-6 shadow-md">
+      <Search size={18} />
+
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        className="w-full bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
