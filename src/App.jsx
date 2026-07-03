e03dea525869a9d5e8b7160a3b7cd1dbe95a6df8
Ali <ali.shameli1380@yahoo.com>
2026-07-03 22:56:16 +0330

    fix: countryDetail path chenged from 'name' to 'countryName' for fix useParams

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Route path="/" element={<Home />} />
 
       <Route
-        path="/country/:name"
+        path="/country/:countryName"
         element={<CountryDetails />}
       />
     </Routes>
