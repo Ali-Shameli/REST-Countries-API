5c83c7c76c72bed92f0800e1cb8687d350684c42
Ali <ali.shameli1380@yahoo.com>
2026-07-03 22:36:47 +0330

    feat(data): taking country data from service

--- src/Pages/CountryDetails.jsx
+++ src/Pages/CountryDetails.jsx
@@ -1,12 +1,26 @@
 import { useParams } from "react-router-dom";
+import { getCountryByName } from "../services/countries";
+
+import Header from "../components/Header";
+import { useEffect, useState } from "react";
 
 const CountryDetails = () => {
 
-  const {name} = useParams()
-  console.log(name)
-  return (
-    <h1>Country Details</h1>
-  );
+  const {countryName} = useParams()
+
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+  async function fetchCountry() {
+    const data = await getCountryByName(countryName);
+    setCountry(data);
+  }
+
+  fetchCountry();
+}, [countryName]);
+
+  if (!country) return <p>Loading...</p>;
+
 };
 
 export default CountryDetails;
